3c42d917110f434ddba9f2fbf3975cfac4365fb1
anshul mishra <anshul7898@yhaoo.com>
2026-03-21 11:21:18 +0530

    Added new Pages

--- src/pages/DashboardComponent.js
+++ src/pages/DashboardComponent.js
@@ -2,20 +2,23 @@ import ComingSoon from '../Components/FeatureCommingSoon';
 import Sidebar from '../Components/Sidebar';
 import { useState } from 'react';
 import { Box } from '@mui/material';
+
 const DashboardComponent = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="dashboard">
-      <Box sx={{ position: 'relative', width: '100%' }}>
-        {/* Sidebar */}
-        <Sidebar
-          open={sidebarOpen}
-          onToggle={() => setSidebarOpen(!sidebarOpen)}
-        />
+    <Box sx={{ position: 'relative', width: '100%' }}>
+      {/* Sidebar */}
+      <Sidebar
+        open={sidebarOpen}
+        onToggle={() => setSidebarOpen(!sidebarOpen)}
+      />
+
+      {/* Main Content */}
+      <Box sx={{ p: 3, background: '#fafbfc', minHeight: '100vh' }}>
+        <ComingSoon />
       </Box>
-      <ComingSoon />
-    </div>
+    </Box>
   );
 };
 
